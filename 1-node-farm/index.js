41f098eb5b0bbdee43b899ed70b09c82ac1b5766
tonpasso <wellpasso@gmail.com>
2021-08-20 08:46:18 -0300

    filling the templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -6,21 +6,21 @@ const url = require("url");
 // FILES
 
 // Blocking, synchronous way
-// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf8');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf8');
 // console.log(textIn);
 // const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${new Date()}`;
-// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
 // Non-Blocking, asynchronous way
 
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //     console.log(data2);
-//     fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
 //       console.log(data3);
 
-//     fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}. \nCreated on ${new Date()}`, 'utf-8', err => {
+//     fs.writeFile('./txt/final.txt', `${data2}\n${data3}. \nCreated on ${new Date()}`, 'utf-8', err => {
 //       console.log('Your file has been written!')
 //     })
 //     });
@@ -30,16 +30,44 @@ const url = require("url");
 
 /////////////////////////////////
 // SERVER
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf8');
+///{%PRODUCTNAME%}/g = all the placeholders will be replaced
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf8');
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
 // dataObj is an array with the 5 objects that came from data.json
-const dataObs = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   // Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the Overview Page!");
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const htmlCards = dataObj.map(element => replaceTemplate(tempCard, element)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', htmlCards);
+
+    res.end(output);
 
   // Product Page
   } else if (pathName === "/product") {
