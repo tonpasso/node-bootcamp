e40e1f2c2fe5210fc85905a1c2fc16805a45a4f4
tonpasso <wellpasso@gmail.com>
2021-09-01 10:59:15 -0300

    adding comments

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -17,6 +17,7 @@ fs.readFile("test-file.txt", () => {
 
   process.nextTick(() => console.log("Process nextTick"));
 
+  // Asynchronous
   // crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
   //   console.log(Date.now() - start, "Password encrypted");
   // });
