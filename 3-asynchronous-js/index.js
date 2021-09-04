324f612177791d42f673a4cf6bf1f3b41304ddec
tonpasso <wellpasso@gmail.com>
2021-09-03 21:31:16 -0300

    the problems with callbacks

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -1,6 +1,20 @@
 const fs = require("fs");
-const superagent = require('superagent');
+const superagent = require("superagent");
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`); 
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile("dog-img.txt", res.body.message, data, (err) => {
+        if (err) reject("Could not write file 😢");
+        console.log("Random dog image saved to file!");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
