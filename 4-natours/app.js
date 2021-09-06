3c707ac07a5d1ad4a8fa72e4010d6fce8b369055
tonpasso <wellpasso@gmail.com>
2021-09-06 07:02:44 -0300

    setting up express and basic routing

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send(console.log('Hello, world!')); 
+})
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('Port:',PORT));
