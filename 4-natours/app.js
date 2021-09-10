4e9aa34a4d761b8a8bc3161234694f97cb0d7dce
tonpasso <wellpasso@gmail.com>
2021-09-10 20:18:27 -0300

    handling POST requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,20 +1,42 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
-const toursInfo = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    status: 'Success',
-    results: toursInfo.length,
+    status: "Success",
+    results: tours.length,
     data: {
-      tours: toursInfo
-    }
+      tours,
+    },
   });
 });
 
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours), err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        }
+      })
+    }
+  );
+});
 
 const PORT = 3001;
-app.listen(PORT, () => console.log('Port:',PORT));
+app.listen(PORT, () => console.log("Port:", PORT));
