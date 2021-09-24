b0e8e296c79ed55c4b080f9d84e6007788549ff8
tonpasso <wellpasso@gmail.com>
2021-09-23 22:19:59 -0300

    connecting database with the express app

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,  
+}).then(() => console.log('DB connection successful')
+)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("App running on port:", PORT));
