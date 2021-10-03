fdf26932d8753b583909c5f20adf3ebb25bcf613
tonpasso <wellpasso@gmail.com>
2021-10-03 10:35:16 -0300

    refecrefactoring API features

--- 4-natours/controller/tourController.js
+++ 4-natours/controller/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 const aliasTopTours = (req, res, next) => {
   req.query.limit='5';
@@ -12,63 +13,62 @@ const getAllTours = async (req, res) => {
     console.log('req query', req.query);
     // First we build the query
     // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach(element => delete queryObj[element]);
+    // const queryObj = { ...req.query };
+    // const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    // excludeFields.forEach(element => delete queryObj[element]);
     // console.log('query obj', queryObj);
 
     // 1B) Advanced Filtering
-    const queryString = JSON.stringify(queryObj); 
-    const newQuery = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // const queryString = JSON.stringify(queryObj); 
+    // const newQuery = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
      // operators to replace -> gte, gt, lte, lt
     // console.log(JSON.parse(newQuery));    
     
-    let query = Tour.find(JSON.parse(newQuery));    
+    // let query = Tour.find(JSON.parse(newQuery));    
 
     // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
       // console.log(sortBy);
-      query = query.sort(sortBy);
+      // query = query.sort(sortBy);
       // sort('price ratingsAverage name)
-    } else {
-      query = query.sort('name')
-    }
+    // } else {
+    //   query = query.sort('name')
+    // }
 
     // 3) Fields limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
       // query = query.select('name duration price')
-    } else {
-      query = query.select('-__v')
-    }
+    // } else {
+    //   query = query.select('-__v')
+    // }
 
     // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page -1) * limit;
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page -1) * limit;
 
     // page=2&limit=10 -> 1-10 is page 1, 11-20 is page 2, 21-30 is page 3...
     // query = query.skip(10).limit(10) -> it will skip the first 10 results and show the page number 2
-    query = query.skip(skip).limit(limit);
+    // query = query.skip(skip).limit(limit);
 
     // If the number of the page dos not exists
-    if (req.query.page) {
-      const numbersOfTours =  await Tour.countDocuments();
+    // if (req.query.page) {
+    //   const numbersOfTours =  await Tour.countDocuments();
       // console.log(numbersOfTours)
-      if (skip >= numbersOfTours) throw new Error('This page does not exist');
-    } 
+    //   if (skip >= numbersOfTours) throw new Error('This page does not exist');
+    // } 
 
-    // Then we execute the query
-    const tours = await query;
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
 
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equalls('easy')
-    // console.log(req.query, queryObj);
+    const tours = await features.query;
 
     // Send response
     res.status(200).json({
