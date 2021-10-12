148032f83597853b8f77e7ecd85cae11ca937b43
tonpasso <wellpasso@gmail.com>
2021-10-12 09:17:38 -0300

    controller e model

---
+++ natours/routes/tourRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const tourController = require('./../controller/tourController');
+
+const router = express.Router();
+// router.param('id', tourController.checkId);
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+  .route('/tour-stats')
+  .get(tourController.getTourStats);
+
+router
+  .route('/monthly-plan/:year')
+  .get(tourController.monthlyPlanTours);
+
+router
+.route('/')
+.get(tourController.getAllTours)
+.post(tourController.createTour);
+
+router
+.route('/:id')
+.get(tourController.findTourById)
+.patch(tourController.updateTour)
+.delete(tourController.excludeTour);
+
+module.exports = router;
