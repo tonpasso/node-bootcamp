b3a6af387990443b6b2bac059a5dce37533c715e
tonpasso <wellpasso@gmail.com>
2021-10-19 10:12:31 -0300

    creating new users and managing passwords

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userController = require('./../controller/userController');
+const authController = require('./../controller/authController');
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
 
 router
  .route('/')
